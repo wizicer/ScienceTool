7db1c631fecc6804aee8b0c6b430e25aff98945a
LiangShuang <liangshuang@sutu.tech>
2023-03-09 16:05:13 +0800

    link to source code

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,6 +7,9 @@
       <p>
         这里是我收集的一些科研用的工具，都是纯网页，可以离线使用的，欢迎使用并提出反馈。
       </p>
+      <p>
+        <a target="_blank" href="https://github.com/wizicer/ScienceTool">源代码</a>
+      </p>
     </article>
   </div>
 </template>
