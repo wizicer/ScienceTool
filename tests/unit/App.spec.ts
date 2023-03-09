e529ab1046917bfe7bc54ba172ebb3a0071e1a85
LiangShuang <liangshuang@sutu.tech>
2023-03-09 14:50:22 +0800

    fix test

--- tests/unit/App.spec.ts
+++ tests/unit/App.spec.ts
@@ -5,7 +5,7 @@ describe('App.vue', () => {
   it('has header', () => {
     const wrapper = mount(App)
     expect(wrapper.html()).toMatch(
-      '<h1 class="text-4xl font-bold my-6">Hello Vue 3 + TypeScript + Vite + TailwindCSS</h1>'
+      '<router-link to="/">科研工具</router-link>'
     )
   })
 })
