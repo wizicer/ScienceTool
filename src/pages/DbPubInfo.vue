60e5ef77499b1e32bdc9deb62d7005ae4267a211
Icer Liang <liangshuang@sutu.tech>
2024-09-10 09:14:32 +0800

    updated

--- src/pages/DbPubInfo.vue
+++ src/pages/DbPubInfo.vue
@@ -22,7 +22,28 @@ import { Ref, ref } from 'vue'
             <a href="https://2025.sigmod.org/" target="_blank">2025 官网</a>
             <a href="https://dl.acm.org/loi/pacmmod" target="_blank">论文集</a>
           </td>
-          <td>四轮：1/4/7/10月</td>
+          <td>
+            <p>Round 1:</p>
+            <ul>
+              <li>Weds, Jan 10, 2024: Abstract Submission and Declaration of COIs</li>
+              <li>Weds, Jan 17, 2024: Paper Submission</li>
+            </ul>
+            <p>Round 2:</p>
+            <ul>
+              <li>Weds, Apr 10, 2024: Abstract Submission and Declaration of COIs</li>
+              <li>Weds, Apr 17, 2024: Paper Submission</li>
+            </ul>
+            <p>Round 3:</p>
+            <ul>
+              <li>Weds, Jul 10, 2024: Abstract Submission and Declaration of COIs</li>
+              <li>Weds, Jul 17, 2024: Paper Submission</li>
+            </ul>
+            <p>Round 4:</p>
+            <ul>
+              <li>Thurs, Oct 10, 2024: Abstract Submission and Declaration of COIs</li>
+              <li>Thurs, Oct 17, 2024: Paper Submission</li>
+            </ul>
+          </td>
         </tr>
         <tr>
           <td><abbr title="International Conference on Very Large Data Bases">VLDB</abbr></td>
@@ -34,13 +55,28 @@ import { Ref, ref } from 'vue'
           <td><abbr title="International Conference on Data Engineering">ICDE</abbr></td>
           <td>2025.11.10 Wellington, New Zealand</td>
           <td><a href="https://www.icdeworldconference2025.com/" target="_blank">2025 官网</a></td>
-          <td>2023年有两轮，分别在7月和11月</td>
+          <td>
+            <p>First Round:</p>
+            <ul>
+              <li>Submission due: August 2, 2024</li>
+            </ul>
+            <p>Second Round:</p>
+            <ul>
+              <li>Submission due: November 25, 2024</li>
+            </ul>
+          </td>
         </tr>
         <tr>
           <td><abbr title="International World Wide Web Conferences">WWW</abbr></td>
           <td>2025.4.28 Sydney, Australia</td>
           <td><a href="https://www2025.org/" target="_blank">2025 官网</a></td>
-          <td>2023年有一轮，在10月</td>
+          <td>
+            重要日期：
+            <ul>
+              <li>摘要提交截止日期：2024年10月7日</li>
+              <li>全文提交截止日期：2024年10月14日</li>
+            </ul>
+          </td>
         </tr>
       </tbody>
     </table>
